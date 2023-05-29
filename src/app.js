92dffd9a1b3891185c81f2432ea2940549f0b69c
MalikUsman92 <malikusmansa@gmail.com>
2023-05-29 16:23:24 +0500

    Express Installed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
-console.log('hello Usman');
+var express = require('express');
+var app = express();
 const abc = require('uuid');
 v4 = abc.v4();
 v3 = abc.v3;
 console.log(v4);
 console.log(v3.DNS);
+
+app.listen(8000, ()=>{
+    console.log("Server listening at port http://localhost:8000")
+})
